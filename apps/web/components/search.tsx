3976833883161289ff558958c20bc2b276f2bc31
aoian <4039293@qq.com>
2025-04-05 01:21:42 +0800

    select added filterIcon

--- apps/web/components/search.tsx
+++ apps/web/components/search.tsx
@@ -20,7 +20,10 @@ export function Search(props: Props) {
         <div className="w-full rounded-2xl flex items-center gap-2 px-4">
           <Select>
             <SelectTrigger className="w-[180px] !h-10 border-none shadow-none bg-input/40">
-              <SelectValue placeholder="All" />
+              <div className="inline-flex gap-2 items-center">
+                <Filter />
+                <SelectValue placeholder="All" />
+              </div>
             </SelectTrigger>
             <SelectContent>
               <SelectItem className="py-2" value="all">All</SelectItem>
@@ -29,7 +32,7 @@ export function Search(props: Props) {
             </SelectContent>
           </Select>
           <div className="w-full relative">
-            <SearchFirst strokeWidth={2} className="absolute size-4 top-1/2 left-2 -translate-y-1/2" />
+            <SearchFirst className="absolute size-4 top-1/2 left-2 -translate-y-1/2" />
             <Input type="email" placeholder="Search 1000 Icons" className="pl-8 h-10 border-none bg-input/40 focus-visible:ring-[1px]" />
           </div>
         </div>
